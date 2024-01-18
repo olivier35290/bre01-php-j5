5396ac42383c96439aa4743adc8507a0b4471d71
olivier35290 <olivier.charlet@neuf.fr>
2024-01-18 16:13:56 +0000

    fin

--- logic/delete-user.php
+++ logic/delete-user.php
@@ -1,6 +1,20 @@
 <?php
-/**
- * @author : Gaellan
- * @link : https://github.com/Gaellan
- */
 
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+}
+
+require "../config/connexion.php";
+
+$query = $db->prepare("DELETE FROM users WHERE id = :id");
+$parameters = [
+    "id" => $id
+    ];
+
+$query-> execute($parameters);
+
+header('Location: https://oliviercharlet.sites.3wa.io/php/bre01-php-j5/pdo/bre01-php-j5/index.php');
+exit;
+
+
+?>
